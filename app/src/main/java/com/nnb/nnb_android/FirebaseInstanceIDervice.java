5fbd7ec2c70bf332ea9807c25e6c9c9cf260aff5
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2021-10-02 22:29:04 +0900

    유재만 : token 갱신 되었을 경우에도 sharedPreference에 데이터 올리도록 로직 추가

--- app/src/main/java/com/nnb/nnb_android/FirebaseInstanceIDervice.java
+++ app/src/main/java/com/nnb/nnb_android/FirebaseInstanceIDervice.java
@@ -1,5 +1,6 @@
 package com.nnb.nnb_android;
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -16,8 +17,18 @@ public class FirebaseInstanceIDervice extends FirebaseInstanceIdService {
         String token = FirebaseInstanceId.getInstance().getToken();
         Log.e(TAG, token);
 
+        setToken(token);
+
         sendRegistrationToServer(token);
 
+    }
+
+    // 값 저장하기
+    public void setToken(String token) {
+        SharedPreferences pref = getSharedPreferences("token", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("token", token);
+        editor.commit();
     }
 
     private  void sendRegistrationToServer( String token){
